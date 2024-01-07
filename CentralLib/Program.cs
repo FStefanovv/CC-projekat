39b19ee11824d762eff22c0977aab0d25de618f6
Filip <filipficastefanov22@gmail.com>
2024-01-07 19:33:02 +0100

    changed from localhost to 0.0.0.0

--- CentralLib/Program.cs
+++ CentralLib/Program.cs
@@ -23,12 +23,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+/*
 using (var scope = app.Services.CreateScope())
 {
     var dbContext = scope.ServiceProvider
         .GetRequiredService<CentralDbContext>();
     dbContext.Database.Migrate();
 }
+*/
 
 
 // Configure the HTTP request pipeline.
